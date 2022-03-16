f1431f578b29980d43948fe8f14e6f923622abba
Shane Danaher <shane@Keiths-MacBook-Pro.local>
2022-03-16 12:10:26 -0700

    [FEAT] Add GTM

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -8,6 +8,7 @@ import styles from './Layout.module.scss'
 // Components
 import Header from 'components/Header'
 import Footer from 'components/Footer'
+import Script from 'next/script'
 
 const Layout:FC = ({ children }) => {
   const dispatch = useAppDispatch()
@@ -27,6 +28,21 @@ const Layout:FC = ({ children }) => {
 
   return (
     <div className={styles.meta_wrap}>
+      {/* GTM  */}
+      <Script
+        strategy='afterInteractive'
+        id='gtm'
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PGSR9BL');
+          `
+        }}
+      />
+      {/* End GTM  */}
       <div className={styles.content_wrap}>
         <Header />
         <main>
