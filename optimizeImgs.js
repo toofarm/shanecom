2d0fdb17077bc94d9a98bcd7cb2e695e76677c7c
toofarm <toofarm@gmail.com>
2024-02-09 15:07:54 -0800

    linting

--- optimizeImgs.js
+++ optimizeImgs.js
@@ -1,48 +1,63 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 const sharp = require('sharp')
 
-async function optimizeImages(inputDirectory, outputDirectory, options) {
+async function optimizeImages(
+  inputDirectory, outputDirectory, options
+) {
   try {
     // Create output directory if it doesn't exist
     if (!fs.existsSync(outputDirectory)) {
-      fs.mkdirSync(outputDirectory);
+      fs.mkdirSync(outputDirectory)
     }
 
     // Read the list of files in the input directory
-    const files = fs.readdirSync(inputDirectory);
+    const files = fs.readdirSync(inputDirectory)
 
     // Loop through each file in the input directory
     for (const file of files) {
-      const inputFilePath = path.join(inputDirectory, file);
-      const outputFilePath = path.join(outputDirectory, file);
+      const inputFilePath = path.join(
+        inputDirectory, file
+      )
+      const outputFilePath = path.join(
+        outputDirectory, file
+      )
 
       // Check if the current file is an image (you may want to add more image file extensions)
       if (file.match(/\.(jpg|jpeg|png)$/i)) {
         // Process and optimize the image using sharp
         await sharp(inputFilePath)
-          .resize(options.width, options.height)
+          .resize(
+            options.width, options.height
+          )
           .jpeg({ quality: options.quality })
-          .toFile(outputFilePath);
+          .toFile(outputFilePath)
 
-        console.log(`Image optimized: ${file}`);
+        console.log(`Image optimized: ${file}`)
       } else {
         // If it's not an image, simply copy the file to the output directory
-        fs.copyFileSync(inputFilePath, outputFilePath);
+        fs.copyFileSync(
+          inputFilePath, outputFilePath
+        )
 
-        console.log(`File copied: ${file}`);
+        console.log(`File copied: ${file}`)
       }
     }
 
-    console.log('Optimization completed.');
+    console.log('Optimization completed.')
   } catch (error) {
-    console.error('Error optimizing images:', error);
+    console.error(
+      'Error optimizing images:', error
+    )
   }
 }
 
-optimizeImages('./public/uploads', './public/uploads/compressed', {
-  width: 800,
-  height: 600,
-  quality: 80
-})
+optimizeImages(
+  './public/uploads', './public/uploads/compressed', {
+    width: 800,
+    height: 600,
+    quality: 80
+  }
+)
